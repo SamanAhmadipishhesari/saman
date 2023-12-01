327f5e6f311fbfd62dcfce27a53b9db0f5105e17
samanahmadil <samanahmadi1372@gmail.com>
2023-12-01 17:08:54 +0330

    add my progect

---
+++ typescript.ts
@@ -0,0 +1,13 @@
+// const test = 'saman'
+// console.log(test)
+
+// const inputs = document.querySelectorAll('input')
+// console.log(inputs )
+
+let string = 'saman'
+let number = 30
+let boolean = true
+
+// boolean = 4
+
+let decimal: number = 6;
